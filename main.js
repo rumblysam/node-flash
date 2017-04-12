f56b54ba65ef7088d2c9857736cc5672f166a811
Samantha Smith <11sms88@gmail.com>
2017-04-11 23:40:16 -0500

    adding inquirer prompt

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
-var mysql = require('inquirer');
+var inquirer = require('inquirer');
 var cards = require('./cards.json');
 
 //console.log((require('./cards.json')));
 
-var question = cards.first_card.front;
+// cards.forEach(function(card) {
+//     console.log(card.front);
+// })
 
-console.log(question);
+
+//var question = cards.first_card.front;
+// var arr = [];
+// for (var id in cards) {
+//     arr.push(cards[question]);
+// }
+
+// We need to use a contructor, 
+//but I didn't fully understand this in class.
+//
+
+//console.log(question);
+
+inquirer.prompt([
+    {
+        "type":"input",
+        "message": cards[0].front,
+        "name":"answer"
+    },
+
+]).then(function (answers) {
+    if (answers.answer == cards[0].back) {
+        console.log("horray!");
+    }
+    //if answers.answer is equal to back
+    // Use user feedback for... whatever!! 
+});
